7d3faf94352fe01aeffe66d2b3c326c971f2c168
Ryan Reece <ryan.reece@cern.ch>
2016-08-10 10:58:02 +0200

    ci

--- scripts/transform_tex.py
+++ scripts/transform_tex.py
@@ -116,9 +116,9 @@ def main():
                     fpath = reo.group(1)
                     suffix = reo.group(2)
                     if os.path.isfile('%s.pdf' % fpath):
-                        newline = '\\includegraphics[width=0.9\linewidth,height=0.9\linewidth,keepaspectratio]{%s.pdf}\n' % fpath
+                        newline = '\\includegraphics[width=1.0\linewidth,height=0.75\linewidth,keepaspectratio]{%s.pdf}\n' % fpath
                     else:
-                        newline = '\\includegraphics[width=0.9\linewidth,height=0.9\linewidth,keepaspectratio]{%s.%s}\n' % (fpath, suffix)
+                        newline = '\\includegraphics[width=1.0\linewidth,height=0.75\linewidth,keepaspectratio]{%s.%s}\n' % (fpath, suffix)
                         
 
             f_out.write(newline)
