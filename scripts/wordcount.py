589ff06c43cab85934e0149a8ab20fee42459a17
Ryan Reece <ryan.reece@cern.ch>
2016-08-31 20:26:42 +0200

    ci

--- scripts/wordcount.py
+++ scripts/wordcount.py
@@ -85,6 +85,7 @@ def main():
     ## make plot
     ax = df['Pages'].plot(marker='o',markersize=8)
 #    ax.set_xlabel("Date")
+    ax.set_xlabel("")
     ax.set_ylabel("Pages")
     fig = ax.get_figure()
     fig.savefig('pages.png')
@@ -93,6 +94,7 @@ def main():
 
     ax = df['Words'].plot(marker='o',markersize=8)
 #    ax.set_xlabel("Date")
+    ax.set_xlabel("")
     ax.set_ylabel("Words")
     fig = ax.get_figure()
     fig.savefig('words.png')
